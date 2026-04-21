ade5984b646afea87fda620e0e078188680cda06
ssmirnovacode <svetlana.smirnova213@gmail.com>
2026-04-21 15:36:17 +0200

    mode corrected

--- app.py
+++ app.py
@@ -15,15 +15,16 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
-LITE_MODE = True
+LITE_MODE = False
 
 load_dotenv(override=True)
 
-username = "ed-donner"
-dataset = f"{username}/items_lite" if LITE_MODE else f"{username}/items_full"
+if __name__ == "__main__":
+    username = "ed-donner"
+    dataset = f"{username}/items_lite" if LITE_MODE else f"{username}/items_full"
 
-train, val, test = Item.from_hub(dataset)
+    train, val, test = Item.from_hub(dataset)
 
-print(
-    f"Loaded {len(train):,} training items, {len(val):,} validation items, {len(test):,} test items"
-)
+    print(
+        f"Loaded {len(train):,} training items, {len(val):,} validation items, {len(test):,} test items"
+    )
