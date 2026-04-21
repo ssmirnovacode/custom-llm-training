18bc59debd4e2373ef15be36db37cc8e1d394ef3
ssmirnovacode <svetlana.smirnova213@gmail.com>
2026-04-21 15:26:02 +0200

    Added evaluator and items model

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from huggingface_hub import login
+from evaluator import evaluate
+from litellm import completion
+from items import Item
+import numpy as np
+from tqdm.notebook import tqdm
+import csv
+from sklearn.feature_extraction.text import HashingVectorizer
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+LITE_MODE = True
+
+load_dotenv(override=True)
+
+username = "ed-donner"
+dataset = f"{username}/items_lite" if LITE_MODE else f"{username}/items_full"
+
+train, val, test = Item.from_hub(dataset)
+
+print(
+    f"Loaded {len(train):,} training items, {len(val):,} validation items, {len(test):,} test items"
+)
